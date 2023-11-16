c075ab82427bdb22d2636c2bda3a244c98d838fb
enableusserver <149377808+enableusserver@users.noreply.github.com>
2023-11-16 14:37:05 +0530

    Update test2.js

--- test2.js
+++ test2.js
@@ -43,6 +43,9 @@ function assign2(arr){
   else if(arr[0] == 'Ben  Wright'){
       return 'Nicolas Comin Marques';
   }
+  else{
+    return 'Nicolas Comin Marques';
+  }
 }
 // funtion to calculate user to assign task and oppertunity
 function assign(arr){
